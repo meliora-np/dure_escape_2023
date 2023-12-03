aeb8b33922b5208d90004d790f0d139d271bf88d
meliora-np <152794932+meliora-np@users.noreply.github.com>
2023-12-04 02:03:08 +0900

    added input field and check correct answer thing

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 document.getElementById("toggleDarkMode").addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
 });
+
+function checkAnswer() {
+    // Get the user's input
+    var userAnswer = document.getElementById("answerInput").value;
+
+    // Define correct answers (you can customize this array)
+    var correctAnswers = ["answer1", "answer2", "answer3"];
+
+    // Check if the user's answer is in the array of correct answers
+    if (correctAnswers.includes(userAnswer)) {
+        displayResult("Correct!");
+    } else {
+        displayResult("Incorrect. Please try again.");
+    }
+}
+
+function displayResult(message) {
+    // Display the result message
+    document.getElementById("resultMessage").textContent = message;
+}
