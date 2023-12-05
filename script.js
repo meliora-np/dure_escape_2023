d6792b1b7832bff28d6f26b55c6345d89fad2360
meliora-np <152794932+meliora-np@users.noreply.github.com>
2023-12-05 19:21:55 +0900

    000

--- script.js
+++ script.js
@@ -2,22 +2,24 @@ document.getElementById("toggleDarkMode").addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
 });
 
+function navigateToStory(storyPage) {
+    window.location.href = storyPage;
+}
+
 function checkAnswer() {
-    // Get the user's input
-    var userAnswer = document.getElementById("answerInput").value;
-
-    // Define correct answers (you can customize this array)
-    var correctAnswers = ["answer1", "answer2", "answer3"];
-
-    // Check if the user's answer is in the array of correct answers
-    if (correctAnswers.includes(userAnswer)) {
-        displayResult("Correct!");
+    var userInput = document.getElementById('answerInput').value.toLowerCase(); // assuming answers are case-insensitive
+    if (userInput === 'correctanswer') {
+        // Allow access to the next stage
+        window.location.href = 'next-stage.html';
     } else {
-        displayResult("Incorrect. Please try again.");
+        // Display an error message or take other actions
+        alert('Incorrect answer. Try again.');
     }
 }
+
 
 function displayResult(message) {
     // Display the result message
     document.getElementById("resultMessage").textContent = message;
 }
+
