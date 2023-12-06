7191c47b08f808c8cc52c658beae75c4355ff977
meliora-np <152794932+meliora-np@users.noreply.github.com>
2023-12-06 12:35:25 +0900

    0

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-document.getElementById("toggleDarkMode").addEventListener("click", function() {
-    document.body.classList.toggle("dark-mode");
-});
-
 function navigateToStory(storyPage) {
     window.location.href = storyPage;
 }
@@ -17,9 +13,42 @@ function checkAnswer() {
     }
 }
 
+// Function to check user progress and redirect accordingly
+/* function checkUserProgress() {
+    var userProgress = localStorage.getItem('userProgress');
+
+    if (userProgress === null) {
+        // User is at the beginning of the story
+        window.location.href = 'index.html';
+    } else if (userProgress === 'stage1') {
+        // User has completed stage 1, redirect to the next stage
+        window.location.href = 'stage2.html';
+    } else if (userProgress === 'stage2') {
+        // User has completed stage 2, redirect to the next stage
+        window.location.href = 'stage3.html';
+    } // Add more conditions as needed
+}
+
+
+// Function to update user progress
+function updateUserProgress(nextStage) {
+    localStorage.setItem('userProgress', nextStage);
+} */
+
 
 function displayResult(message) {
     // Display the result message
     document.getElementById("resultMessage").textContent = message;
 }
 
+function startLaptopStory() {
+    console.log('startLaptopStory function called');
+    window.location.href = 'laptop-story-1.html';
+}
+
+function submitAnswer() {
+    // Logic to check the answer
+    // If correct, navigate to the next stage
+    window.location.href = 'laptop-story-2.html';
+    // If incorrect, show a message or handle accordingly
+}
